ad944a768c796b12a232e338dbfade7203e906fc
José Fresco <josemariafresco@gmail.com>
2016-12-14 16:30:26 -0300

    Make constructor be able to receive a `bugsnag` instance (that could be `bugsnag-node`)

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ const warn = () => window !== 'undefined' &&
   typeof window.Bugsnag !== 'object' &&
   console.warn('redux-bugsnag has been executed but it seems like Bugsnag snippet has not been loaded.')
 
-export default () => tap(filterError, (error, action, store) => {
-  typeof window !== 'undefined' && typeof window.Bugsnag === 'object'
-    ? window.Bugsnag.notifyException(error)
-    : warn()
+export default (bugsnag) => tap(filterError, (error, action, store) => {
+  if (!bugsnag && typeof window !== 'undefined' && typeof window.Bugsnag === 'object') {
+    bugsnag = window.Bugsnag
+  } else if (bugsnag) {
+    typeof bugsnag.notifyException === 'function' ? bugsnag.notifyException(error) : bugsnag.notify(error)
+  } else {
+    return warn()
+  }
 })
