5e10d6c9215f366e37c8f5d46a87ee597ce96042
José Fresco <josemariafresco@gmail.com>
2016-12-14 16:31:51 -0300

    Make warning message to be aware only of the existence of `console`

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import tap from 'redux-tap'
 
 const filterError = ({ meta }) => meta && meta.error
 
-const warn = () => window !== 'undefined' &&
-  typeof window.Bugsnag !== 'object' &&
+const warn = () => console &&
   console.warn('redux-bugsnag has been executed but it seems like Bugsnag snippet has not been loaded.')
 
 export default (bugsnag) => tap(filterError, (error, action, store) => {
