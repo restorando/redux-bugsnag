82985318636ba1297269078c9c1841d8a6b5c505
José Fresco <josemariafresco@gmail.com>
2016-12-15 15:21:59 -0300

    Make filter configurable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import tap from 'redux-tap'
 
-const filterError = ({ meta }) => meta && meta.error
+const defaultFilter = ({ meta }) => meta && meta.error
 
 const warn = () => console &&
   console.warn('redux-bugsnag has been executed but it seems like Bugsnag snippet has not been loaded.')
 
-export default (bugsnag) => tap(filterError, (error, action, store) => {
+export default (bugsnag, { filterProperty = defaultFilter }) => tap(filterProperty, (error, action, store) => {
   if (bugsnag) {
     typeof bugsnag.notifyException === 'function' ? bugsnag.notifyException(error) : bugsnag.notify(error)
   } else {
